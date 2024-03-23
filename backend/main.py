0ecd350d2c1fdfbb32bda2725256e26b26707842
Ethan Ermovick <keg9ve@virginia.edu>
2024-03-23 17:16:08 -0400

    add support for getting campaigns via /get_campaigns

--- backend/main.py
+++ backend/main.py
@@ -1,63 +1,51 @@
 import json
 
-from flask import Flask, render_template, url_for
+from flask import Flask, jsonify
 
-from backend.Objects.Hospital import Hospital
-from backend.Objects.Patient import Patient
+from backend.Objects.Campaign import Campaign
 
 app = Flask(__name__)
 
 
-@app.route("/hospitals")
-def hospitals():
-    pass
-
-@app.route("/home")
-def home():
-    return render_template('home.html', posts=posts)
-
-
-@app.route("/about")
-def about():
-    return render_template('about.html', title='About')
-
-
-def write_hospital_json(hospitals):
-    hospitals_serial = [hospital.get_serial() for hospital in hospitals]
-    jsn = json.dumps(hospitals_serial, indent=4)
-    with open("Data/Hospitals.json", "w") as outfile:
+def write_campaign_json(campaigns):
+    serial_data = [d.get_serial() for d in campaigns]
+    jsn = json.dumps(serial_data, indent=4)
+    with open("Data/Campaigns.json", "w") as outfile:
         outfile.write(jsn)
 
 
-def get_hospitals():
-    with open("Data/Hospitals.json", "r") as infile:
-        hospitals_serial = json.load(infile)
-        hospitals = [Hospital(hospital_serial) for hospital_serial in hospitals_serial]
-        return hospitals
+def get_campaigns_data(jsn=True):
+    with open("Data/Campaigns.json", "r") as infile:
+        serial_data = json.load(infile)
+        if jsn:
+            return serial_data
+        cpns = [Campaign(d) for d in serial_data]
+        return cpns
 
 
-def write_patient(patient):
-    jsn = json.dumps(patient.get_serial(), indent=4)
-    with open(f"Data/Patients/{patient.id}.json", "w") as outfile:
-        outfile.write(jsn)
 
-
-def get_patient_from_id(patient_id):
-    with open(f"Data/Patients/{patient_id}.json", "r") as infile:
-        patient_serial = json.load(infile)
-        patient = Patient(patient_serial)
-        return patient
+@app.route("/get_campaigns")
+def get_campaigns():
+    return jsonify(get_campaigns_data())
 
 
 if __name__ == '__main__':
-    test_hospital = Hospital()
-    test_hospital.name = "test hospital"
-    test_hospital.location = "ukraine"
-    test_hospital.patients = ["patient_1"]
-    test_hospital.curr_donations = 5
-    test_hospital.donation_goal = 10
-    hospitals = [test_hospital]
-    write_hospital_json(hospitals)
+    tc = Campaign()
+    tc.start = "3/22/2024"
+    tc.end = "3/25/2024"
+    tc.name = "Ukraine Hosptial war relif"
+    tc.description = "desc"
+    tc.hospital_name = "ukr nat hospital"
+    tc.pledge_amount = 10000000000000
+    tc.city = "Kiev"
+    tc.country = "Ukraine"
+    tc.contact_email = "someone@gmail.com"
+    tc.paypal = "@paypaluser"
+    tc.name = "test hospital"
+    tc.location = "ukraine"
+    tc.patients = ["patient_1"]
+    campaigns = [tc]
+    write_campaign_json(campaigns)
     # jsn = json.dumps(hospitals, indent=4)
     #
     # # Writing to sample.json
