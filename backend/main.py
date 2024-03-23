b26e9c02192f0c1f1eb1b989881d556d6d02a8ad
Ethan Ermovick <keg9ve@virginia.edu>
2024-03-23 13:50:21 -0400

    setup patient/hospital objects

--- backend/main.py
+++ backend/main.py
@@ -1,25 +1,17 @@
-from flask import Flask, render_template, url_for
-app = Flask(__name__)
+import json
 
-posts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
-]
+from flask import Flask, render_template, url_for
+
+from backend.Objects.Hospital import Hospital
+from backend.Objects.Patient import Patient
+
+app = Flask(__name__)
 
 
 @app.route("/hospitals")
 def hospitals():
-    
+    pass
+
 @app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
@@ -30,5 +22,45 @@ def about():
     return render_template('about.html', title='About')
 
 
+def write_hospital_json(hospitals):
+    hospitals_serial = [hospital.get_serial() for hospital in hospitals]
+    jsn = json.dumps(hospitals_serial, indent=4)
+    with open("Data/Hospitals.json", "w") as outfile:
+        outfile.write(jsn)
+
+
+def get_hospitals():
+    with open("Data/Hospitals.json", "r") as infile:
+        hospitals_serial = json.load(infile)
+        hospitals = [Hospital(hospital_serial) for hospital_serial in hospitals_serial]
+        return hospitals
+
+
+def write_patient(patient):
+    jsn = json.dumps(patient.get_serial(), indent=4)
+    with open(f"Data/Patients/{patient.id}.json", "w") as outfile:
+        outfile.write(jsn)
+
+
+def get_patient_from_id(patient_id):
+    with open(f"Data/Patients/{patient_id}.json", "r") as infile:
+        patient_serial = json.load(infile)
+        patient = Patient(patient_serial)
+        return patient
+
+
 if __name__ == '__main__':
+    test_hospital = Hospital()
+    test_hospital.name = "test hospital"
+    test_hospital.location = "ukraine"
+    test_hospital.patients = ["patient_1"]
+    test_hospital.curr_donations = 5
+    test_hospital.donation_goal = 10
+    hospitals = [test_hospital]
+    write_hospital_json(hospitals)
+    # jsn = json.dumps(hospitals, indent=4)
+    #
+    # # Writing to sample.json
+    # with open("Data/Hospitals.json", "w") as outfile:
+    #     outfile.write(jsn)
     app.run(debug=True)
